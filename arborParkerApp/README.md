74d5d2d8ee043503b69b7f9259b413465d8d6e9b
Julia <jwoelmer@umich.edu>
2022-10-02 20:06:43 -0400

    Created android project and deleted swift one

--- arborParkerApp/README.md
+++
@@ -1,3 +0,0 @@
-# arborParkerApp
-
-A description of this package.
--- arborParkerApp/Sources/arborParkerApp/arborParkerApp.swift
+++
@@ -1,6 +0,0 @@
-public struct arborParkerApp {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
