7311177a5e4914275594a0fc4caa7c541488437d
Jessica <jesszhu@umich.edu>
2022-10-04 19:15:09 -0400

    view profile

---
+++ app/src/main/java/com/example/arborparker/ViewProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.handyopinion.ProfileActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arborparker.R
+
+class ViewProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
