9a53eaf2366f581863288d01f1534dd6bd53bbbe
Amber Mahmood <ambermah@umich.edu>
2022-10-30 18:42:45 -0400

    combined main acitvity view classes and commented out error handling. users can create profiles but no error handling

--- app/src/main/java/com/example/arborparker/CreateProfileActivity.kt
+++ app/src/main/java/com/example/arborparker/CreateProfileActivity.kt
@@ -1,33 +1,82 @@
 package com.example.arborparker
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.arborparker.network.User
+import timber.log.Timber
 
 
 class CreateProfileActivity : AppCompatActivity() {
 
-    lateinit var etUserName: EditText
-    lateinit var etPassword:EditText
+    //lateinit var etUserName: EditText
+    //lateinit var etPassword: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
 
+        //connects to buttons
         var btn_create = findViewById(R.id.btn_create) as Button
         var btn_backtologin = findViewById(R.id.btn_backtologin) as Button
 
         btn_create.setOnClickListener {
-            startActivity(Intent(this, MapsActivity::class.java));
+            //connects to text blocks
+            var get_user_name = findViewById(R.id.et_user_name) as EditText
+            var get_password = findViewById(R.id.et_password) as EditText
+
+            // gets text from input
+            val usernametxt = get_user_name.text.toString()
+            val passwordtxt = get_password.text.toString()
+
+            val userInfo = User(//id = null,
+                                username = usernametxt,
+                                password = passwordtxt)
+
+            // adds user
+            val apiNetwork = MainActivityViewModel()
+            apiNetwork.addUser(userInfo) {
+                if (it?.username != null) {
+                    // it = newly added user parsed as response
+                    // it?.id = newly added user ID
+
+                } else {
+                    Timber.d("Error registering new user")
+
+                    // error handling for when user already exists
+                    // still fixing errors
+                    /**
+                    val alertDialogBuilder = AlertDialog.Builder(this)
+                    alertDialogBuilder.setTitle("Android user alert")
+                    alertDialogBuilder.setMessage("That username is already taken. Please choose another.")
+                    alertDialogBuilder.setCancelable(true)
+
+                    alertDialogBuilder.setPositiveButton(android.R.string.yes) { _,_ ->
+                        Toast.makeText(applicationContext,
+                            android.R.string.yes, Toast.LENGTH_SHORT).show()
+                    }
+
+                    val alert1: AlertDialog = alertDialogBuilder.create()
+                    alert1.show()
+                    **/
+                }
+
+                // takes user to map screen
+                startActivity(Intent(this, MapsActivity::class.java))
+            }
+
         }
 
         btn_backtologin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java));
+
+            // takes user back to login screen
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 
--- app/src/main/java/com/example/arborparker/MainActivityViewModel.kt
+++ app/src/main/java/com/example/arborparker/MainActivityViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.arborparker
 
+import android.service.autofill.UserData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.arborparker.network.RetrofitClient
+import com.example.arborparker.network.RetrofitClient.retrofit
 import com.example.arborparker.network.Spot
+import com.example.arborparker.network.User
 import kotlinx.coroutines.launch
+import retrofit2.Callback
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivityViewModel : ViewModel() {
 
@@ -16,4 +22,19 @@ class MainActivityViewModel : ViewModel() {
             spotList.value = RetrofitClient.retrofit.getSpots()
         }
     }
+
+    fun addUser(userData: User, onResult: (User?) -> Unit){
+        retrofit.addUser(userData).enqueue(
+            object : Callback<User> {
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<User>, response: Response<User>) {
+                    val addedUser = response.body()
+                    onResult(addedUser)
+                }
+            }
+        )
+    }
+
 }
